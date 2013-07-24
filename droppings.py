9125de15d7899afe0c8538ba9f446cb1e1290b69
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-07-24 15:44:17 -0700

    Initial commit of klutz.

---
+++ droppings.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os, sys
+import os.path
+import yaml
+from subprocess import call
+
+def exec_cmd(cmd, good_status=0):
+    print "-"*80
+    printable_cmd = " ".join(cmd)
+    print "Command: " + printable_cmd
+    status = call(cmd, stdout=sys.__stdout__, stderr=sys.__stderr__)
+
+    if status != good_status:
+        sys.__stderr__.write(
+            "ERROR: '%s' exited with a status of %i\n" % (printable_cmd, status)
+        )
+        sys.exit(status)
+    else:
+        return status
+
+class Git(object):
+    def __init__(self, refspec):
+        self.refspec = refspec
+
+    def clone(self, directory):
+        if os.path.exists(directory):
+            print directory + " already exists, skipping clone."
+            return 0
+        else:
+            cmd = ["git", "clone", self.refspec, directory]
+            return exec_cmd(cmd)
+
+    def checkout(self, identifier):
+        return exec_cmd(["git", "checkout", identifier])
+
+    def pull(self, pull_branch, repo="origin"):
+        self.checkout(pull_branch)
+        return exec_cmd(["git", "pull", repo, pull_branch])
+
+    def merge(self, from_branch, to_branch, repo="origin"):
+        self.checkout(from_branch)
+        self.pull(from_branch, repo=repo)
+        self.checkout(to_branch)
+        self.pull(to_branch, repo=repo)
+        return exec_cmd(["git", "merge", from_branch])
+
+    def fetch(self):
+        return exec_cmd(["git", "fetch"])
+
+    def push(self, push_branch, repo="origin"):
+        self.checkout(push_branch)
+        return exec_cmd(["git", "push", repo, push_branch])
+
+if __name__ == "__main__":
+    g = Git("git@github.com:iPlantCollaborativeOpenSource/Donkey.git")
+    g.clone("donkey")
+    first_dir = os.getcwd()
+    os.chdir("donkey")
+    g.merge("dev", "donkzilla")
+    g.push("donkzilla")
+    os.chdir(first_dir)
---
+++ provision.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export LEIN_ROOT="1"
+export DEBIAN_FRONTEND=noninteractive
+
+install-lein () {
+    if [ ! -f /usr/local/bin/lein ]; then
+        echo "Downloading lein"
+        wget https://raw.github.com/technomancy/leiningen/stable/bin/lein 2>&1 lein-download-log
+
+        echo "Moving lein into /usr/local/bin"
+        mv lein /usr/local/bin/
+
+        echo "Making lein executable"
+        chmod a+x /usr/local/bin/lein
+
+        echo "Running lein for the first time"
+        lein --help
+    else
+        echo "lein is already installed at /usr/local/bin/lein"
+    fi
+}
+
+apt-get update
+apt-get install -y openjdk-6-jre-headless git curl screen python python-pip vim
+pip install pyyaml
+install-lein
+
